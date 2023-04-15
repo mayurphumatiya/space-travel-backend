4b3f9753b30a8b6954fcf735d98869e28a1180d3
mayurphumatiya <mayurphumatiya13@gmail.com>
2023-04-15 20:57:07 +0530

    crypting password while storing

--- api/userRoutes.js
+++ api/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../models/User";
+import bcrypt from "bcryptjs";
 
 const userRouter = express.Router();
 
@@ -19,15 +20,16 @@ userRouter.get("/", async (req, res, next) => {
   return res.status(200).json({ users });
 });
 
+//SIGN UP
 userRouter.post("/signup", async (req, res, next) => {
   let { first_name, last_name, email, password } = req.body;
   if (!first_name && !last_name && !email && !password) {
     return res.status(422).res.json({ message: "Invalid inputs" });
   }
-
+  const hashPassword = bcrypt.hashSync(password);
   let user;
   try {
-    user = new User({ first_name, last_name, email, password });
+    user = new User({ first_name, last_name, email, password: hashPassword });
     user = await user.save();
   } catch (e) {
     console.log(e);
