e580054dea327a777047c891a23b6234b3c582c9
mayurphumatiya <mayurphumatiya13@gmail.com>
2023-05-31 15:31:54 +0530

    destination by id api created

--- routes/destinationRoutes.js
+++ routes/destinationRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { auth } from "../middlewares/auth";
-import { addDestination, getDestinations } from "../controllers/destinationController";
+import { addDestination, getDestinations, getDestinationsById } from "../controllers/destinationController";
 
 const destinationRouter = express.Router();
 
 destinationRouter.post("/add", addDestination);
 
 destinationRouter.get("/", getDestinations)
+
+destinationRouter.get("/:id", getDestinationsById)
 
 export default destinationRouter;
