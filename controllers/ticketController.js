40da1c3b28396975eeb822427f985f306d059b91
mayurphumatiya <mayurphumatiya13@gmail.com>
2023-06-23 17:32:30 +0530

    tickets model changed

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -5,67 +5,77 @@ import { isFieldPresentInRequest, sendMail } from "../utils/helpers";
 
 export const ticketBooking = async (req, res) => {
   try {
-    let mail ="mayurphumatiya6@gmail.com";
-    sendMail(mail)
-    // const reqBody = req.body;
-    // let requiredFields = [
-    //   "ticket",
-    //   "destination",
-    //   "est_time",
-    //   "distance",
-    //   "total_price",
-    // ];
-    // let invalidFields = [];
+    const reqBody = req.body;
+    let requiredFields = [
+      "full_name",
+      "destination",
+      "tickets",
+      "price",
+      "total_price",
+      "travel",
+      "distance",
+      "image",
+      "email",
+      "paymentMethod",
+    ];
+    let invalidFields = [];
 
-    // // Condition to check if required fields are present in the request
-    // // TODO: Make it more dynamic by iterating through a list of objects instead where we can specify if a field is optional, requires regex validation, check character limit count etc.
-    // requiredFields.forEach((field) => {
-    //   if (!isFieldPresentInRequest(reqBody, field)) {
-    //     invalidFields.push(field);
-    //   }
-    // });
+    // Condition to check if required fields are present in the request
+    // TODO: Make it more dynamic by iterating through a list of objects instead where we can specify if a field is optional, requires regex validation, check character limit count etc.
+    requiredFields.forEach((field) => {
+      if (!isFieldPresentInRequest(reqBody, field)) {
+        invalidFields.push(field);
+      }
+    });
 
-    // if (invalidFields.length > 0) {
-    //   return res.status(200).json({
-    //     status: false,
-    //     message: `Error - Missing fields: ${invalidFields.join(", ")}`,
-    //   });
-    // }
+    if (invalidFields.length > 0) {
+      return res.status(200).json({
+        status: false,
+        message: `Error - Missing fields: ${invalidFields.join(", ")}`,
+      });
+    }
 
-    // let { ticket, destination, est_time, distance, total_price } = reqBody;
+    let { ticket, destination, est_time, distance, total_price } = reqBody;
 
-    // const userId = req.user.id;
-    // const user_id = new mongoose.Types.ObjectId(userId);
+    const userId = req.user.id;
+    const user_id = new mongoose.Types.ObjectId(userId);
 
-    // const user = await User.findOne({ _id: user_id });
-    // if (!user) {
-    //   return res
-    //     .status(200)
-    //     .json({ status: false, message: "User does not exist" });
-    // }
-    // const bookTicket = new Tickets({
-    //   user_id: user._id,
-    //   ticket,
-    //   destination,
-    //   est_time,
-    //   distance,
-    //   total_price,
-    // });
+    const user = await User.findOne({ _id: user_id });
+    if (!user) {
+      return res
+        .status(200)
+        .json({ status: false, message: "User does not exist" });
+    }
+    const bookTicket = new Tickets({
+      user_id: user._id,
+      full_name,
+      destination,
+      tickets,
+      price,
+      total_price,
+      travel,
+      distance,
+      image,
+      email,
+      paymentMethod,
+      cardNo,
+      upiId
+    });
 
-    // const newTicket = await bookTicket.save();
+    const newTicket = await bookTicket.save();
 
-    // if (!newTicket) {
-    //   return res.status(200).json({
-    //     status: false,
-    //     message:
-    //       "There was a problem while booking your ticket, Please try again later!",
-    //   });
-    // }
+    if (!newTicket) {
+      return res.status(200).json({
+        status: false,
+        message:
+          "There was a problem while booking your ticket, Please try again later!",
+      });
+    }
 
-    // return res.status(200).json({
-    //   status: true,
-    //   message: `Congratulations on booking your ticket to the ${newTicket.destination}!`,
-    // });
+    return res.status(200).json({
+      status: true,
+      message: `Congratulations! Your tickets are booked🎉`,
+    });
   } catch (e) {
     console.log(e);
     return res.status(200).json({
